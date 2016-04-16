c50b7cfc26df784fae8e2520d4c6a5099ebf5347
Sidd <ssudhe13@earlham.edu>
2016-04-16 15:36:20 -0500

    changed the app name

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('trump-spotter-app.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/scripts/app.js
+++ www/scripts/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('trump-spotter-app', ['ionic', 'trump-spotter-app.controllers', 'trump-spotter-app.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
